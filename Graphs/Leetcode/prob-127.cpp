df82412a8f797ff9b4967e3848a86e778d5a1fb5
Aayush <aayushbisht501@gmail.com>
2023-02-09 11:03:42 +0530

    added leetcode problem 127

---
+++ Graphs/Leetcode/prob-127.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string>s(wordList.begin(),wordList.end());
+        s.erase(beginWord);
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
+        while(!q.empty()){
+            string word = q.front().first;
+            int ans = q.front().second;
+            if(word == endWord) return ans;
+            q.pop();
+            for(int i=0;i<word.size();i++)
+            {
+                char ini = word[i];
+                for(char ch = 'a';ch<='z';ch++)
+                {
+                    word[i] = ch;
+                    if(s.find(word)!=s.end())
+                    {
+                        s.erase(word);
+                        q.push({word,ans+1});
+                    }
+                }
+                word[i] = ini;
+            }
+        }
+        return 0;
+    }
+};
