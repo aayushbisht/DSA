e83191a23f821e58b52595f74287517e2d276d82
Aayush <aayushbisht501@gmail.com>
2023-07-26 16:06:36 +0530

    added problem 1492

---
+++ Important_Ques/leet_1492_kthFactor.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+    float root = sqrt(n);
+
+    for (int i=1; i<root; i++) {
+        if (n % i == 0 && --k == 0) 
+            return i;
+    }
+
+    for (int i=root; i>0; i--) {
+        if (n % i == 0 && --k == 0) 
+            return n/i;
+    }
+
+    return -1;
+    }
+};
